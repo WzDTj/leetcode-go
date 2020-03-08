28dcec9f7a324edb540ec2ce34c8095d213d0f29
Dantong Jin <j.dantong@gmail.com>
2020-03-09 01:12:36 +0800

    解决 322. 零钱兑换

---
+++ problems/coin_change.go
@@ -0,0 +1,38 @@
+package problems
+
+import (
+	"math"
+	"sort"
+)
+
+func CoinChange(coins []int, amount int) int {
+
+	sort.Sort(sort.Reverse(sort.IntSlice(coins)))
+
+	result := math.MaxInt32
+	calculateCoins(amount, coins, 0, 0, &result)
+
+	if result == math.MaxInt32 {
+		return -1
+	}
+
+	return result
+}
+
+func calculateCoins(amount int, coins []int, coinIndex int, count int, result *int) {
+
+	if amount == 0 {
+		*result = int(math.Min(float64(count), float64(*result)))
+		return
+	}
+	if coinIndex >= len(coins) {
+		return
+	}
+
+	for i := amount / coins[coinIndex]; i >= 0 && i+count < *result; i-- {
+		remainder := i * coins[coinIndex]
+
+		calculateCoins(amount-remainder, coins, coinIndex+1, count+i, result)
+
+	}
+}
