8c71a3dd4e94ae16629e301687ff51a33cacbb3f
Dantong Jin <j.dantong@gmail.com>
2020-03-09 15:19:47 +0800

    解决 121. 买卖股票的最佳时机

---
+++ problems/max_profit.go
@@ -0,0 +1,21 @@
+package problems
+
+import (
+	"math"
+)
+
+/**
+ * 递推：
+ * 1、依次计算最大售价
+ * 2、依次计算最大利润
+ **/
+func MaxProfit(prices []int) int {
+	var maxProfit, maxSellPrice float64
+
+	for i := len(prices) - 2; i >= 0; i-- {
+		maxSellPrice = math.Max(maxSellPrice, float64(prices[i+1]))
+		maxProfit = math.Max(maxProfit, maxSellPrice-float64(prices[i]))
+	}
+
+	return int(maxProfit)
+}
